72686d14f4f0471791b1b2a5b2991c52912e0773
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 11:19:23 +0530

    //https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=aW5xWG1sNHNpdm1pQmdsNUh5K2tZdz09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09 #include<bits/stdc++.h> using namespace std; int main() { 	string s; 	while(cin >> s) { 		cout << s << " "; 	} }

---
+++ Take_Input_And_Print.c
@@ -0,0 +1,9 @@
+//https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=aW5xWG1sNHNpdm1pQmdsNUh5K2tZdz09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	string s;
+	while(cin >> s) {
+		cout << s << " ";
+	}
+}
