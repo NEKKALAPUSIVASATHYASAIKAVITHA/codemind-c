e8c96391dd7282823cd9f38a9bb813876a0aec65
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 10:38:00 +0530

    //https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=ZG5wcHJhOFRsRkVUSUEzZE5vcFU4QT09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09 #include<stdio.h> int main() {     float fc;     scanf("%f",&fc);     printf("%.2f",((fc - 32.0)*(5/9.0))); }

---
+++ Fahrenheit_to_Celsius.c
@@ -0,0 +1,7 @@
+//https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=ZG5wcHJhOFRsRkVUSUEzZE5vcFU4QT09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09
+#include<stdio.h>
+int main() {
+    float fc;
+    scanf("%f",&fc);
+    printf("%.2f",((fc - 32.0)*(5/9.0)));
+}
