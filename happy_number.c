4b4dd9efeeefa27982358b708a71d7b5b36d381e
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 12:08:47 +0530

    #include<bits/stdc++.h> using namespace std; int main() { 	int n; cin >> n; 	while (n > 9) { 		int sum = 0; 		while (n) { 			sum += pow(n % 10, 2); 			n /= 10; 		} 		n = sum; 	} 	if (n == 1 or n == 7) cout << "True"; 	else cout << "False"; }

--- happy_number.c
+++ happy_number.c
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    int n;
-    cin >> n;
-    while(n > 9) {
-        int r = 0;
-        while(n) {
-            int rem = n % 10;
-            n /= 10;
-            r += rem* rem;
-        }
-        n = r;
-    }
-    if(n==1 or n == 7) cout << "True";
-    else cout << "False";
+	int n; cin >> n;
+	while (n > 9) {
+		int sum = 0;
+		while (n) {
+			sum += pow(n % 10, 2);
+			n /= 10;
+		}
+		n = sum;
+	}
+	if (n == 1 or n == 7) cout << "True";
+	else cout << "False";
 }
