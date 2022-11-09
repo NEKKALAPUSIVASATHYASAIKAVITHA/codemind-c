898e36d0826ef7918e1d658f4bcd903005db8398
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 11:58:32 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,r;     cin >> n >> r;     for(int i = 1; i <= r; i++) {         if(i%2 == 0)continue;         cout << n << " x " << i << " = " << n*i << endl;     } }

---
+++ Tables.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main() {
+    int n,r;
+    cin >> n >> r; 
+    for(int i = 1; i <= r; i++) {
+        if(i%2 == 0)continue;
+        cout << n << " x " << i << " = " << n*i << endl;
+    }
+}
+
