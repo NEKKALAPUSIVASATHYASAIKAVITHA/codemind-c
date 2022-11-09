2f7fc6170d7f9c65eb3a47220857602e2d000a8e
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 12:10:50 +0530

    #include<bits/stdc++.h> using namespace std; bool ispalindrome(int n) { 	int rev = 0; 	while (n > rev) { 		rev = rev * 10 + n % 10; 		n /= 10; 	} 	return rev == n or rev / 10 == n; } int main() { 	int n; 	cin >> n; 	int l = n - 1, r = n + 1; 	while (1) { 		if (ispalindrome(l) and ispalindrome(r)) { 			cout << l << " " << r; 			break; 		} 		else if (ispalindrome(l)) { 			cout << l; 			break; 		} 		else if (ispalindrome(r)) { 			cout << r; 			break; 		} 		l--; 		r++; 	} }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,33 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int reverse(int n) {
-    int rev = 0;
-    while(n) {
-        rev = rev * 10 + n % 10;
-        n /= 10;
-    }
-    return rev;
+bool ispalindrome(int n) {
+	int rev = 0;
+	while (n > rev) {
+		rev = rev * 10 + n % 10;
+		n /= 10;
+	}
+	return rev == n or rev / 10 == n;
 }
 int main() {
-    int n;
-    cin >> n;
-    int l = n - 1, r = n + 1;
-    while(1) {
-        int revL = reverse(l);
-        int revR = reverse(r);
-        if(revL == l and revR == r) {
-            cout << l << " " << r;
-            break;
-        }
-        else if(revL == l) {
-            cout << l;
-            break;
-        }
-        else if(revR == r) {
-            cout << r;
-            break;
-        }
-        l--;
-        r++;
-    } 
+	int n;
+	cin >> n;
+	int l = n - 1, r = n + 1;
+	while (1) {
+		if (ispalindrome(l) and ispalindrome(r)) {
+			cout << l << " " << r;
+			break;
+		}
+		else if (ispalindrome(l)) {
+			cout << l;
+			break;
+		}
+		else if (ispalindrome(r)) {
+			cout << r;
+			break;
+		}
+		l--;
+		r++;
+	}
 }
