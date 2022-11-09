28765517ac87e338c465b2d060033fd31d31f1f3
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 10:51:11 +0530

    //https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=SkVIMTNGeXJpc0N5ejJFQVlJODJKUT09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09 #include<bits/stdc++.h> using namespace std; int main() { 	int x , y; 	cin >> x >> y; if((x%2 == 0 && y%2 == 0) || (x%2 == 0 && y%2 == 1 && x > 1)) cout << "YES"; 	else cout << "NO"; }

---
+++ Can_share_equally?.c
@@ -0,0 +1,9 @@
+//https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=SkVIMTNGeXJpc0N5ejJFQVlJODJKUT09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x , y;
+	cin >> x >> y;
+if((x%2 == 0 && y%2 == 0) || (x%2 == 0 && y%2 == 1 && x > 1)) cout << "YES";
+	else cout << "NO";
+}
