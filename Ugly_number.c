c5f1ae6a4dfb6124ffca666cd93ab599466f5d3d
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 12:06:42 +0530

    #include<bits/stdc++.h> using namespace std; int main() { 	int n; cin >> n; 	if (n == 1) cout << "Ugly Number"; 	else { 		while (n % 2 == 0) { 			n /= 2; 		} 		while (n % 3 == 0) { 			n /= 3; 		} 		while (n % 5 == 0) { 			n /= 5; 		} 		if (n != 1) cout <<  "Not Ugly Number"; 		else cout << "Ugly Number"; 	} }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-	int n;
-	cin >> n;
-	while(n%2 == 0) n/= 2;
-	while(n%3 == 0) n /=3;
-	while(n%5 == 0) n /= 5;
-	if(n == 1) cout << "Ugly Number";
-	else cout << "Not Ugly Number";
+	int n; cin >> n;
+	if (n == 1) cout << "Ugly Number";
+	else {
+		while (n % 2 == 0) {
+			n /= 2;
+		}
+		while (n % 3 == 0) {
+			n /= 3;
+		}
+		while (n % 5 == 0) {
+			n /= 5;
+		}
+		if (n != 1) cout <<  "Not Ugly Number";
+		else cout << "Ugly Number";
+	}
 }
