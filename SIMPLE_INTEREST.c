a2ff528ed15a81f5d399e5bb893b8149aaabb839
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-11-09 10:38:43 +0530

    //https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=UmlNMlRtNGVQazNmb0cvcDFBZ0llZz09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09 #include<stdio.h> int main() {     int p,t,r;     scanf("%d%d%d",&p,&t,&r);     printf("%d",(p*t*r)/100); }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,7 @@
+//https://www.thecodemind.io/app/discription.php?pageCategory=c3pzTm1NaHFsYWVCeFpGMVpkTDloZz09&Tid=eDdqYzJidml5cGFLMEhQNThJMHowdz09&Pid=UmlNMlRtNGVQazNmb0cvcDFBZ0llZz09&Course=bCt3WlAvSHJ0UFZNQ1lYanRHWm5lQT09&Technology=VytvRXpaWXBDM1pCeDVxWTd6QkQzZz09&Topic=SXVxYURxU2Y1N2VDd2E3d1hpVExMZz09
+#include<stdio.h>
+int main() {
+    int p,t,r;
+    scanf("%d%d%d",&p,&t,&r);
+    printf("%d",(p*t*r)/100);
+}
